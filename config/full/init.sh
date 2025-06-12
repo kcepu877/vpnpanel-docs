1767f5d529d0758cd9f144492e3a83bdbdc161b9
Gege Desembri <gegedesembri.643.um@gmail.com>
2025-06-12 16:38:13 +0700

    Remove deprecated bot API and configuration files; update docker-compose for improved service management

--- config/full/init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Set permission supaya file bisa dibaca oleh nginx
-
-echo "[init] Setting read permissions in /var/lib/telegram-bot-api..."
-chmod -R o+r /var/lib/telegram-bot-api || echo "[init] Warning: Gagal chmod"
-
-echo "[init] Done."
-
