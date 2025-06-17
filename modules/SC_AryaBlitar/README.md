93a15d083897d9ef7c2bd2840487e6c228fb2608
Gege Desembri <GegeDesembri.643.UM@gmail.com>
2025-06-17 22:51:21 +0700

    Add SC_AryaBlitar module with installation script and usage instructions

---
+++ modules/SC_AryaBlitar/README.md
@@ -0,0 +1,16 @@
+# SC_AryaBlitar
+
+## Cara Menggunakan
+
+1. **Pastikan Anda berada di direktori modul:**
+    ```bash
+    wget -qO- https://raw.githubusercontent.com/GegeDevs/vpnpanel-docs/refs/heads/main/modules/SC_AryaBlitar/install.sh | bash
+    ```
+
+## Catatan
+
+- Pastikan Anda memiliki hak akses root.
+
+## Bantuan
+
+Jika mengalami kendala, silakan buat issues.
---
+++ modules/SC_AryaBlitar/install.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+source_code="GegeDevs/vpnpanel-docs"
+module_name="SC_AryaBlitar"
+bin_dir="/etc/gegevps/bin"
+
+if [[ "$EUID" -ne 0 ]]; then
+    echo "The script must be run as root." >&2
+    exit 1
+fi
+
+function link_gen(){
+    dl_link="https://raw.githubusercontent.com/${source_code}/refs/heads/main/modules/${module_name}/telegram-${1}-${2}.sh"
+    echo "${dl_link}"
+}
+
+function install_sh(){
+    full_link="$(link_gen ${1} ${2})"
+    file_name="$(echo "${full_link}" | rev | cut -d'/' -f 1 | rev)"
+    wget -qO "${bin_dir}/${file_name}" "${full_link}"
+}
+
+function tunnels_list(){
+    echo "
+        sshvpn
+        vmess
+        vless
+        ssocks
+    " | sed 's/^[ \t]*//g;/^$/d'
+}
+
+function actions_list(){
+    echo "
+        create
+        extend
+    " | sed 's/^[ \t]*//g;/^$/d'
+}
+
+tunnels_list | while read -r tunnel; do
+    if [[ ! -d "${bin_dir}" ]]; then
+        mkdir -p "${bin_dir}"
+    fi
+    actions_list | while read -r action; do
+        install_sh "${tunnel}" "${action}"
+    done
+done
